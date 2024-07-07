c7a8c765a304a58ca34516dd1b8a2775717d22c4
Rouge script <hamzasuleiman7xa7@gmail.com>
2024-07-07 20:26:44 +0100

    crud

---
+++ index.js
@@ -0,0 +1,137 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const Product = require('./models/product.model.js');
+mongoose
+  .connect(
+    "mongodb+srv://himxa:VWpmYZ73du9pZ5kc@cluster0.wtzsrez.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("connected"))
+  .catch((err) => console.log(err));
+
+
+ 
+  
+  app.use(express.json());
+  app.get('/',(req,res)=>{
+
+
+    res.send('welcome to home page')
+  })
+  
+
+
+
+
+
+
+
+
+
+  app.post('/api/product',async(req,res)=>{
+
+
+    try {
+      const product = await Product.create(req.body)
+      res.status(201).json(product);
+      
+    } catch (error) {
+      res.status(500).json({message: error.message});
+    }
+
+  })
+
+
+
+  app.get('/api/product',async(req,res)=>{
+
+
+    try {
+      const product = await Product.find({})
+      res.status(201).json(product);
+      
+    } catch (error) {
+      res.status(500).json({message: error.message});
+    }
+
+  })
+  
+  
+
+//getting perticular element
+
+  app.get('/api/product/:id',async(req,res)=>{
+const {id} = req.params
+
+    try {
+     // getting peticular element by id from db
+      const product = await Product.findById(id)
+      res.status(201).json(product);
+      
+    } catch (error) {
+      res.status(500).json({message: error.message});
+    }
+
+  })
+  
+
+
+ // updating doc
+
+ app.put('/api/product/:id', async(req,res)=>{
+  const {id} = req.params
+  
+      try {
+       // getting peticular element by id from db
+        const product = await Product.findByIdAndUpdate(id,req.body)
+    if(!product){
+
+      return res.status(404).send('NO Item Found')
+    }
+
+    const updatedProduct = await Product.findById(id)
+    res.status(201).json(updatedProduct);
+      } catch (error) {
+        res.status(500).json({message: error.message});
+
+        
+      }
+  
+    })
+
+
+
+
+
+
+    //deleting doc
+
+
+
+    app.delete('/api/product/:id', async(req,res)=>{
+      const {id} = req.params
+      
+          try {
+           // getting peticular element by id from db
+            const product = await Product.findByIdAndDelete(id)
+        if(!product){
+    
+          return res.status(404).send('NO Item Found')
+        }
+    
+        
+        res.status(201).json({message: 'deleted succesfully'});
+          } catch (error) {
+            res.status(500).json({message: error.message});
+    
+            
+          }
+      
+        })
+    
+
+
+
+  app.listen(5000,()=>{
+    console.log('listing to port 5000')
+  })
---
+++ models/product.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Productschema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please enter your product name"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    image: {
+      type: String,
+      required:false,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", Productschema);
+
+module.exports = Product;
